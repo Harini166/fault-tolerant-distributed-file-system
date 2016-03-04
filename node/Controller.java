94e406c90f7b4a34690caeef1b457e843527cd8d
pandarih <harinipandari.3@gmail.com>
2016-03-04 15:17:34 -0700

    Added main to Controller class. Read's command line arguments and starts listening to new connections as a separate thread.

--- node/Controller.java
+++ node/Controller.java
@@ -1,5 +1,6 @@
 package node;
 
+import transport.TCPServer;
 import wireformats.Event;
 import wireformats.Protocol;
 
@@ -12,5 +13,24 @@ public class Controller implements Node, Protocol {
     @Override
     public void handleEvent(Event e, Socket s) {
 
+    }
+
+    public static void main (String [] args) {
+        // TODO: Better way to parse command line arguments. Eg. using Apache commons library.
+        int port = 0;
+        if (args.length == 1) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid input: Port " + args[0] + " must be an integer.");
+            }
+        } else {
+            System.err.println("Invalid input: Please provide a port number.");
+            System.exit(0);
+        }
+
+        Controller controller = new Controller();
+        TCPServer server = new TCPServer(port, controller);
+        new Thread(server).start();
     }
 }
